ec57c1bf611739891c26dfcc06b085d5086532d8
Selam A. <selamsasnake@gmail.com>
2024-12-17 22:55:58 +0300

    fix: resolved plotting issue

--- scripts/stock_indicators.py
+++ scripts/stock_indicators.py
@@ -12,9 +12,12 @@ def calculate_technical_indicators(data):
     ##Relative Strength Index
     data['RSI'] = talib.RSI(data['Close'], timeperiod=50)
 
-    # ##MACD (Moving Average Convergence Divergence)
-    # macd, macdsignal = talib.MACD(data['Close'], signalperiod=50)
-    # data['MACD'] = macd
+    ##MACD (Moving Average Convergence Divergence)
+    macd, macdsignal, _ = talib.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+
+    # Add the MACD and Signal line to your DataFrame
+    data['MACD'] = macd
+    data['MACDSignal'] = macdsignal
 
 def calculate_daily_returns(data):
     data['Daily Returns'] = data['Close'].pct_change()
