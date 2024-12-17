4e25e5c89931bad29688ec4799fa8cdbe0a91320
Selam A. <selamsasnake@gmail.com>
2024-12-17 15:45:12 +0300

    updated eda modules

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,5 +1,8 @@
 import pandas as pd 
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import seaborn as sns
+import matplotlib.pylab as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MaxNLocator
 
 
 def descriptive_statistics(data):
@@ -12,11 +15,12 @@ def descriptive_statistics(data):
     print("Average Headline Length: ", average_length)
 
     length_range = data['headline_length'].max() - data['headline_length'].min()
+    print("max: ",  data['headline_length'].max())
+    print("min: ",  data['headline_length'].min())
     print("Headline Length Range: ", length_range)
 
-
 def article_per_publisher(data):
-    popular_publishers = data.groupby('publisher').size().sort_values(ascending=False)
+    popular_publishers = data.groupby('publisher').size().sort_values(ascending=True)
     return popular_publishers
 
 def analyze_publication_trends(data):
@@ -24,35 +28,18 @@ def analyze_publication_trends(data):
     publications_by_month = data['date'].dt.to_period('M').value_counts().sort_index()
     publications_by_year = data['date'].dt.to_period('Y').value_counts().sort_index()
 
-    print("Publications by Year:")
-    print(publications_by_year)
+    # print("Publications by Year:")
+    # print(publications_by_year)
     
-    print("\nPublications by Month:")
-    print(publications_by_month)
+    # print("\nPublications by Month:")
+    # print(publications_by_month)
     
     # print("\nPublications by Day of the Month:")
     # print(publications_by_day)
 
-## Text Analysis
-def analyze_headline_sentiment(data):
-    analyzer = SentimentIntensityAnalyzer()
+    return publications_by_day, publications_by_month, publications_by_year
 
-    def classify_sentiment(compound_score):
-        if compound_score >= 0.05:
-            return 'positive'
-        elif compound_score <= -0.05:
-            return 'negative'
-        else:
-            return 'neutral'
-    
-    data['sentiment_score'] = data['headline'].apply(lambda x: analyzer.polarity_scores(x)['compound'])
-    data['sentiment'] = data['sentiment_score'].apply(classify_sentiment)
-    
-    return data
-
-   
-
-
-
-
-    
+def analyze_publishing_time(data):
+    data['publication_hour'] = data['date'].dt.hour
+    publication_by_hour = data.groupby('publication_hour').size()
+    print(publication_by_hour)
