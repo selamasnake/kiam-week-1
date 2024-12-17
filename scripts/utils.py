c9122825a9af35091e1e94228ce64f235f351479
Selam A. <selamsasnake@gmail.com>
2024-12-17 20:17:00 +0300

    feat: added stock data utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import os
 
 def load_data_drive(url):
     file_id=url.split('/')[-2]
@@ -26,3 +27,20 @@ def drop_unnamed_column(data):
     # data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
     data = data.drop(columns=['Unnamed: 0'])
     return data.head(5)
+
+##Stock
+
+def extract_ticker(file_path):
+    filename = os.path.basename(file_path)
+    ticker = filename.split('_')[0] 
+    return ticker
+
+def load_stock_data(file_path):
+    ticker = extract_ticker(file_path)
+    data = pd.read_csv(file_path)
+    data.insert(0, 'Ticker', ticker)
+    return data
+
+def index_by_date(data):
+    data['Date'] = pd.to_datetime(data['Date'], 'coerce')
+    data.set_index('Date', inplace=True)
